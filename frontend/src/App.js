e76c4cc488d900a614f59a148750dd7a37fdf523
Anil Shetty <anilshetty3220@gmail.com>
2026-05-16 11:32:56 +0000

    Updated frontend to use nginx reverse proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   // FETCH TASKS
   const fetchTasks = async () => {
-    const response = await fetch("http://34.235.127.232:5000/tasks");
+    const response = await fetch("/api/tasks");
     const data = await response.json();
     setTasks(data);
   };
@@ -18,7 +18,7 @@ function App() {
   // ADD TASK
   const addTask = async () => {
 
-    await fetch("http://34.235.127.232:5000/tasks", {
+    await fetch("/api/tasks", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
